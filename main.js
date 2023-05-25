16dd303b1ad1678a80fc5d0ef9b477c02f88b262
Jahzeel Kiel Peras <jhzlklprs@gmail.com>
2023-05-26 02:53:43 +0800

    creating function getMale

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //Get Btn and Create Function
 document.getElementById('myBtn').addEventListener('click', getData);
+document.getElementById('getMale').addEventListener('click', getMale);
 
 
 function getData() {
@@ -10,7 +11,7 @@ function getData() {
     fetch('https://randomuser.me/api/?results=100')
     .then((res) => res.json())
     .then((data) => {
-        //console.log(data);
+        console.log(data);
 
         let author = data.results;
         // console.log(author);
@@ -21,22 +22,66 @@ function getData() {
         //Get Data Loop Through
         author.forEach(function (lists){
             output += `
-                <div class="container>
+                <div class="container">
                     <div class="card mt-4 bg-light">
                         <ul class="list-group">
                         <li class="list-group-item"><h2>Name: ${lists.name.first}</h2></li>
                         <li class="list-group-item"><img src="${lists.picture.large}"></li>
-                        <li class="list-group-item"><h2>Phone Number: ${lists.name.cell}</h2></li>
+                        <li class="list-group-item"><h2>Phone Number: ${lists.cell}</h2></li>
+                        <li class="list-group-item"><h2>Email ID: ${lists.email}</h2></li>
+                        <li class="list-group-item"><h2>Age: ${lists.dob.age}</h2></li><br><br>
                         </ul>
                     </div>
                 </div>
             
             `;
         });
-    
-        //Show on our screen all data
 
         document.getElementById('output').innerHTML = output;
 
     });
+}
+
+
+
+//Function for getMale
+
+function  getMale() {
+    //Get API
+    fetch('https://randomuser.me/api/?results=100')
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+
+        const male = data.results;
+
+        if(male.gender === "male") {
+            output += `
+            <div class="container">
+                <div class="card mt-4 bg-light">
+                    <ul class="list-group">
+                    <li class="list-group-item"><h2>Name: ${lists.name.first}</h2></li>
+                    <li class="list-group-item"><img src="${lists.picture.large}"></li>
+                    <li class="list-group-item"><h2>Phone Number: ${lists.cell}</h2></li>
+                    <li class="list-group-item"><h2>Email ID: ${lists.email}</h2></li>
+                    <li class="list-group-item"><h2>Age: ${lists.dob.age}</h2></li><br><br>
+                    </ul>
+                </div>
+            </div>
+        
+        `;
+        }
+
+
+        let output = "<h2><center>Get User Data</center></h2>";
+
+
+                
+        //Show on our screen all data
+
+        document.getElementById('output').innerHTML = output;
+        
+    });
+
+
 }
